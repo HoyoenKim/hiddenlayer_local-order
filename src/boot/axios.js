e921725d546981936a09de53fb37984003e812c8
HoyoenKim <cabinkhy@gist.ac.kr>
2023-03-10 19:14:23 +0900

    server ip

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,7 +1,17 @@
 import { boot } from "quasar/wrappers";
 import axios from "axios";
 
-const baseURL = "http://192.168.0.102:3000";
+//const baseURL = "http://localhost:3000";
+//const baseURL = "http://192.168.0.102:3000";
+//const baseURL = "http://210.109.43.14:10005";
+
+// 1. niginx proxy manager (210.109.43.14:81) 에서 192.168.0.82:3000 을 10005 포트로 포워딩 설정
+// BE가 192.168.0.82:3000 에 띄워져 있음
+// 2. 192.168.0.82:22 는 10002 포트로 포워딩 되어 있음
+// BE설정 위해서는 ssh -i pemkey.pem ubuntu@210.109.43.14 -p 10002 로 접속가능
+
+const baseURL = "http://3.36.146.228:3000";
+
 const api = axios.create({ baseURL: baseURL });
 
 export default boot(({ app }) => {
